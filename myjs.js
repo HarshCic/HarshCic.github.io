59c50f5111f98766db5731f45976fbf892b2fb89
Droyo <kharsh.hk89@gmail.com>
2019-12-16 20:22:42 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -24,7 +24,7 @@ function confirmUpload() {
 			'caption': $("#imgDesc").val()
 		},
 	};
-	var uploadTask = firebase.storage().ref().child('photos/'+'1.jpg').put(selectedFile, metadata);
+	var uploadTask = firebase.storage().ref().child('dogImages/' + selectedFile.name).put(selectedFile, metadata);
 	// Register three observers:
 	// 1. 'state_changed' observer, called any time the state changes
 	// 2. Error observer, called on failure
